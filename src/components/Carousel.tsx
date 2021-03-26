7947e7f6881ae367a22047035a70e084512fb5a5
Renato Hideki Osaka <renato.osaka@hotmail.com>
2021-03-26 11:06:55 -0300

    two ways

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,4 +1,4 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 import { useSpringCarousel } from 'react-spring-carousel-js'
 
 import styles from './carousel.module.css';
@@ -24,34 +24,37 @@ export function Carousel({ images }: CarouselProps) {
     }))
   })
 
-  // const [imageIndex, setImageIndex] = useState(0)
+  const [imageIndex, setImageIndex] = useState(0)
   
-  // function nextImage() {
-  //   if (imageIndex < images.length -1) {
-  //     setImageIndex(imageIndex + 1)
-  //   }
-  // }
+  function nextImage() {
+    if (imageIndex < images.length -1) {
+      setImageIndex(imageIndex + 1)
+    }
+  }
 
-  // function priorImage() {
-  //   if (imageIndex > 0) {
-  //     setImageIndex(imageIndex - 1)
-  //   }
-  // }
+  function priorImage() {
+    if (imageIndex > 0) {
+      setImageIndex(imageIndex - 1)
+    }
+  }
 
   return (
-    <div className={styles.container}>
-      {/* {imageIndex > 0 &&  <button className={styles.prevButton} type="button" disabled={imageIndex === 0} onClick={priorImage}/>}
-      
-      <div>
-        <img src={images[imageIndex].url} alt='Nome do Produto'/>
+    <>
+      <div className={styles.container}>
+        <button className={styles.prevButton} type="button" onClick={slideToPrevItem}/>
+        {carouselFragment}
+        <button className={styles.nextButton} type="button" onClick={slideToNextItem}/>
       </div>
+        
+      <div className={styles.container}>
+        {imageIndex > 0 &&  <button className={styles.prevButton} type="button" disabled={imageIndex === 0} onClick={priorImage}/>}
+        
+        <div>
+          <img src={images[imageIndex].url} alt='Nome do Produto'/>
+        </div>
 
-      {imageIndex < images.length -1 && <button className={styles.nextButton} type="button" disabled={imageIndex === images.length -1} onClick={nextImage}/>}
-       */}
-
-      <button className={styles.prevButton} type="button" onClick={slideToPrevItem}/>
-      {carouselFragment}
-      <button className={styles.nextButton} type="button" onClick={slideToNextItem}/>
-    </div>
+        {imageIndex < images.length -1 && <button className={styles.nextButton} type="button" disabled={imageIndex === images.length -1} onClick={nextImage}/>}      
+      </div>
+    </>    
   )
 }
